f3dd1fc1b56fb261908a5335b20fd7be0354a655
Ryan Braun <ryan.m.braun@hotmail.com>
2022-08-20 15:08:48 -0400

    feat: update demo page

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,45 @@
 import React, { useEffect, useState } from 'react'
 import { Carousel } from './components/Carousel'
 
-const MainContainerCss = ({ width }: { width: string }): React.CSSProperties => ({
+const HeaderCss: React.CSSProperties = {
+  fontSize: '3rem',
+}
+
+const SectionTitleCss: React.CSSProperties = {
+  fontSize: '2rem',
+}
+
+const SectionContainerCss: React.CSSProperties = {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+  width: '100%',
+}
+
+const CarouselContainerCss: React.CSSProperties = {
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'center',
   alignItems: 'center',
-  width: `${width}`,
   border: '5px solid gray',
-  gap: '15px',
-  padding: '5px',
-})
+  padding: '15px',
+}
 
 const TileCss = ({ color, width }: { color: string; width: string }): React.CSSProperties => ({
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'center',
   alignItems: 'center',
-  fontSize: '2rem',
+  fontSize: '4rem',
   width: `${width}`,
   height: '300px',
   backgroundColor: `${color}`,
   borderRadius: '5px',
+  color: 'white',
 })
 
-const getRandomColor = (): number => Math.floor(Math.random() * 205) + 50
+const getRandomColor = (): number => Math.floor(Math.random() * 205) + 10
 const randomColors = Array(25)
   .fill(null)
   .map(() => `rgb(${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`)
@@ -34,9 +49,9 @@ function App() {
 
   useEffect(() => {
     const onResize = () => {
-      if (!isMobile && window.innerWidth <= 475) {
+      if (document.body.clientWidth <= 475) {
         setIsMobile(true)
-      } else if (isMobile && window.innerWidth > 475) {
+      } else if (document.body.clientWidth > 475) {
         setIsMobile(false)
       }
     }
@@ -47,34 +62,39 @@ function App() {
     return () => {
       window.removeEventListener('resize', onResize)
     }
-  })
+  }, [])
 
   return (
-    <div
-      style={MainContainerCss({
-        width: isMobile ? '75vw' : '75vw',
-      })}
-    >
-      <Carousel
-        slideWidth={isMobile ? 100 : 200}
-        gridGap={10}
-        displayCount={4}
-        minDisplayCount={isMobile ? 1 : 2}
-        showArrows={!isMobile}
-      >
-        {randomColors.map((color, i) => (
-          <div
-            key={i}
-            style={TileCss({
-              color,
-              width: isMobile ? '100px' : '200px',
-            })}
+    <>
+      <h2 style={HeaderCss}>
+        <u>react-swipeable-infinite-carousel Demo</u>
+      </h2>
+      <br />
+      <p style={SectionTitleCss}>Swipeable, draggable, and scrollable carousel</p>
+      <div style={SectionContainerCss}>
+        <div style={CarouselContainerCss}>
+          <Carousel
+            slideWidth={isMobile ? 80 : 200}
+            gridGap={15}
+            displayCount={isMobile ? 3 : 4}
+            minDisplayCount={isMobile ? 1 : 2}
+            showArrows={!isMobile}
           >
-            {i}
-          </div>
-        ))}
-      </Carousel>
-    </div>
+            {randomColors.map((color, i) => (
+              <div
+                key={i}
+                style={TileCss({
+                  color,
+                  width: isMobile ? '80px' : '200px',
+                })}
+              >
+                {i}
+              </div>
+            ))}
+          </Carousel>
+        </div>
+      </div>
+    </>
   )
 }
 
